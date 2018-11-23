0fa0277ff717a7bbee5a7ba27be6c6a29e8b0ba9
pt2468dev <pt2468dev@gmail.com>
2018-11-24 00:20:42 +1100

    Initial commit

---
+++ source/main.cpp
@@ -0,0 +1,29 @@
+#include <avr/io.h>
+#include <util/delay.h>
+// Class declaration
+class LEDController {
+  public:
+    void init(void);
+    void turnLEDOn(void);
+    void turnLEDOff(void);
+};
+void LEDController::init(void) {
+  // Set PL7 as output port
+  DDRB |= (1<<7);
+}
+void LEDController::turnLEDOn(void) {
+  PORTB |= (1<<7);
+}
+void LEDController::turnLEDOff(void) {
+  PORTB &= ~(1<<7);
+}
+int main(void) {
+  LEDController led_1_controller;
+  led_1_controller.init();
+  while(1) {
+    led_1_controller.turnLEDOn();
+    _delay_ms(500);
+    led_1_controller.turnLEDOff();
+    _delay_ms(500);
+  }
+}
