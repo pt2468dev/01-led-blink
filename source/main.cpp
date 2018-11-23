e397980036dd6285ab8d58a2a901a1f4af5ee3f9
pt2468dev <pt2468dev@gmail.com>
2018-11-24 00:21:24 +1100

    Add README

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
+
 // Class declaration
 class LEDController {
   public:
@@ -7,16 +8,20 @@ class LEDController {
     void turnLEDOn(void);
     void turnLEDOff(void);
 };
+
 void LEDController::init(void) {
   // Set PL7 as output port
   DDRB |= (1<<7);
 }
+
 void LEDController::turnLEDOn(void) {
   PORTB |= (1<<7);
 }
+
 void LEDController::turnLEDOff(void) {
   PORTB &= ~(1<<7);
 }
+
 int main(void) {
   LEDController led_1_controller;
   led_1_controller.init();
